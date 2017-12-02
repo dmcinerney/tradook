98d643879c646646d1b12f7b2b7d5750bc1aacca
Jack Nudelman <jack.nudel@gmail.com>
2017-12-01 23:08:58 -0500

    finished net...i think

--- jack_net.py
+++ jack_net.py
@@ -4,15 +4,16 @@ import torch.nn as nn
 
 class ConvNet(nn.Module):
     def __init__(self, image_size):
-        super(Net, self).__init__()
+        super(ConvNet, self).__init__()
         self.image_size = image_size
-        self.conv_size2 = image_size * 2
+        self.conv_size2 = 64 * 2
 
-        self.conv1 = nn.Conv2d(1, self.image_size, 5, padding=2)
-        self.conv2 = nn.Conv2d(self.image_size, self.conv_size2, 5, padding=2)
+        self.conv1 = nn.Conv2d(3, 64, 5, padding=2)
+        self.conv2 = nn.Conv2d(64, self.conv_size2, 5, padding=2)
+
         self.pool = nn.MaxPool2d(2, 2)
 
-        self.fc1 = nn.Linear(self.conv_size2 * 7 * 7, 100)
+        self.fc1 = nn.Linear(128 * 12 * 12, 100) #128 * 12 * 12 is size (2, 3,4 size indices) after second relu
         self.fc2 = nn.Linear(100, 62)
 
         # self.fc1 = nn.Linear(image_size ** 2, 256)
@@ -20,12 +21,31 @@ class ConvNet(nn.Module):
         # self.fc3 = nn.Linear(128, 62)
 
     def forward(self, x):
-        x = F.relu(self.pool(self.conv1(x)))
-        x = F.relu(self.pool(self.conv2(x)))
-        x = x.view(-1, self.conv_size2 * 7 * 7)
+        x = self.conv1(x)
+        # print 'after conv1', x.size()
+        x = self.pool(x)
+        # print 'after pool', x.size()
+        x = F.relu(x)
+        # print 'after relu', x.size()
+        # print
+        x = self.conv2(x)
+        # print 'after conv2', x.size()
+        x = self.pool(x)
+        # print 'after pool', x.size()
+        x = F.relu(x)
+        # print 'after relu', x.size()
+
+        # x = F.relu(self.pool(self.conv2(x)))
+        # print 'after conv2', x.size()
+        # x = x.view(-1, self.conv_size2 * 7 * 7)
+        x = x.view(-1, self.num_flat_features(x))
+        # print 'after view', x.size()
 
         x = F.relu(self.fc1(x))
+        # print 'after last relu'
         x = self.fc2(x)
+        # print 'after fc2'
+        # print x.size()
 
         return x
         # x = self.fc1(x)
@@ -40,3 +60,10 @@ class ConvNet(nn.Module):
         logits = self.forward(x)
 
         return F.softmax(logits)
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
--- trainer.py
+++ trainer.py
@@ -9,17 +9,33 @@ def get_test_accuracy(net):
     dataset = image_loading(csv_file='test.txt', root_dir='LetterImages/',  transformation = transform)
     dataloader = DataLoader(dataset, batch_size=12, shuffle=True, num_workers=12)
 
+    accuracy = 0
+    iter_num = 1
     for images, labels in iter(dataloader):
-        inputs = Variable(images)
-        target = Variable(labels)
+        if torch.cuda.is_available():
+            inputs = Variable(images, volatile=True).cuda()
+        else:
+            inputs = Variable(images, volatile=True)
 
-        output = net.forward(inputs)
+        predicted = net.predict(inputs).data.cpu()
+        equality = (labels == predicted.max(1)[1])
+        accuracy += equality.type_as(torch.FloatTensor()).mean()
+        iter_num += 1
+    print 'test accuracy: ', float(accuracy) / float(iter_num)
+
+
 
 
 
 def train():
     global image_size
-    net = ConvNet(image_size)
+    if torch.cuda.is_available():
+        print 'running with cuda'
+        net = ConvNet(image_size).cuda()
+        criterion = nn.CrossEntropyLoss().cuda()
+    else:
+        net = ConvNet(image_size)
+        criterion = nn.CrossEntropyLoss()
 
     transform = transforms.Compose([transforms.Scale((net.image_size, net.image_size)), transforms.ToTensor()])
     dataset = image_loading(csv_file='train.txt', root_dir='LetterImages/',  transformation = transform)
@@ -27,8 +43,9 @@ def train():
 
     learning_rate = 1e-4
     optimizer = optim.Adam(net.parameters(), learning_rate)
-    criterion = nn.CrossEntropyLoss()
 
+    train_loss_list = list()
+    testing_accuracy_list = list()
     steps = 1
     running_loss = 0
     print_every = 500
@@ -36,30 +53,52 @@ def train():
         print epoch
         for images, labels in iter(dataloader):
 
-            print type(images)
-            print labels
             labels = np.array(map(int, labels))
             # print type(labels)
-            labels = torch.from_numpy(labels).view(labels.shape[0], -1)
+            labels = torch.from_numpy(labels)
+            # .view(labels.shape[0], -1)
             labels = labels.type(torch.FloatTensor)
-            print labels.size()
+            # print labels.size()
             # image = images.resize(images.size()[0], net.image_size**2)
-            images = images.view(images.numel())
-            inputs = Variable(images)
-            target = Variable(labels)
+            # images = images.view(images.numel())
+            if torch.cuda.is_available():
+                inputs = Variable(images).cuda()
+                target = Variable(labels).cuda()
+            else:
+                inputs = Variable(images)
+                target = Variable(labels)
+            target = target.type(torch.LongTensor)
             optimizer.zero_grad()
-            print inputs.size()
+            # print inputs.size()
             output = net.forward(inputs)
-            print type(output)
+            # print output.size()
+            # print target.size()
+            # print output.size()
+            # print output
+            # print type(output)
             loss = criterion(output, target)
             loss.backward()
             optimizer.step()
 
             running_loss += loss.data[0]
+            train_loss_list.append(loss.data[0])
 
-            # if steps % print_every == 0:
-            #     print 'test accuracy for step #', steps, '= ', get_test_accuracy(net)
+            if steps % print_every == 0:
+                print 'training loss ', float(running_loss) / float(steps)
+                testing_accuracy_list.append(get_test_accuracy(net))
             steps+=1
-            break
+
+    x_training = np.linspace(0, iter_num, len(training_loss_list))
+    plt.plot(x_training, training_loss_list)
+    plt.title('training loss')
+    plt.savefig('jack_training_loss.png')
+    plt.clf()
+
+    x_testing = np.linspace(0, iter_num, len(testing_accuracy_list))
+    plt.plot(x_testing, testing_accuracy_list)
+    plt.title('testing accuracy')
+    plt.savefig('jack_testing_accuracy.png')
+    plt.clf()
+
 
 train()
